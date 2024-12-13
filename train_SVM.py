a6650dbcffcac4c9706721a69a9cf78f57a4b587
TrongNV2003 <trongnguyen220603@gmail.com>
2024-12-13 11:04:48 +0700

    build: logically constructure

--- train_SVM.py
+++ train_SVM.py
@@ -1,39 +1,36 @@
 import time
-import json
-from sklearn.metrics import f1_score
+import argparse
 from training.dataloader import Dataset
-from training.trainer import SVM_Trainer
+from training.trainer import SVM_Trainer, Vectorizer
+from evaluate.evaluate_output import f1, calculate_latency, result_recorder
 
-dataset = Dataset()
+vec = Vectorizer()
 svm = SVM_Trainer()
 
-def f1(vector):
-    prediction = svm.predict(vector)
-    score = f1_score(test_label, prediction, average='macro')
-    print(f"F1-score: {score}")
+parser = argparse.ArgumentParser()
 
-def result_recorder(path):
-    with open(path, "w", encoding="utf-8") as file:
-        json.dump(result, file, indent=4, ensure_ascii=False)
-        print(f"Saved in {path}")
+parser.add_argument("--train_file", type=str, default="dataset/train.json")
+parser.add_argument("--test_file", type=str, default="dataset/test.json")
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    train_set, test_set, evaluate_set = dataset.traditional_approach_dataset()
+    train_set = Dataset(args.train_file)
+    test_set = Dataset(args.test_file)
+    
+    train_text = [train_set[i][0] for i in range(len(train_set))]
+    train_label = [train_set[i][1] for i in range(len(train_set))]
 
-    train_text = train_set['text_processed']
-    train_label = train_set['label']
+    test_text = [test_set[i][0] for i in range(len(test_set))]
+    test_label = [test_set[i][1] for i in range(len(test_set))]
 
-    test_text = test_set['text_processed']
-    test_label = test_set['label']
-
-    train_text_vect = svm.vectorize_train(train_text)
-    test_text_vect = svm.vectorize_test(test_text)
+    train_text_vect = vec.train_vectorizer(train_text)
+    test_text_vect = vec.test_vectorizer(test_text)
     
     start_time = time.time()
     classifier = svm.train(train_text_vect, train_label)
     end_time = time.time()
     process_time = round(end_time - start_time, 6)
-    print(f"Training time: {process_time}")
+    print(f"Training time: {process_time}\n")
 
     result = []
     for test in test_text_vect:
@@ -46,5 +43,7 @@ if __name__ == "__main__":
             "process_time": process_time
         })
 
-    f1(test_text_vect)
-    result_recorder("result/output_svm.json")
+    result_file = "result/output_svm.json"
+    f1(svm, test_text_vect, test_label)
+    result_recorder(result_file, result)
+    calculate_latency(result_file)
