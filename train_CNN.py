7b068e26d2a2c66c5572d4cf9abdf9cc2510ed38
TrongNV2003 <trongnguyen220603@gmail.com>
2024-12-15 02:18:16 +0700

    build: adjust hyper parameters

--- train_CNN.py
+++ train_CNN.py
@@ -6,8 +6,8 @@ from training.models import CNN
 from training.trainer import Trainer
 from training.evaluate import Tester
 from gensim.models import KeyedVectors
-from training.dataloader import Dataset
 from torch.utils.data import DataLoader
+from training.dataloader import Dataset, DatasetCollator
 
 def set_seed(seed: int) -> None:
     random.seed(seed)
@@ -16,36 +16,36 @@ def set_seed(seed: int) -> None:
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--train_file", type=str, default="dataset/train.json")
-    parser.add_argument("--test_file", type=str, default="dataset/test.json")
-    parser.add_argument("--eval_file", type=str, default="dataset/evaluate.json")
-    parser.add_argument("--dict_path", type=str, default="models/wiki.vi.model.bin")
-    parser.add_argument("--epochs", type=int, default=5)
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--batch_size", type=int, default=32)
+parser = argparse.ArgumentParser()
 
-    return parser.parse_args()
+parser.add_argument("--train_file", type=str, default="dataset/train.json")
+parser.add_argument("--test_file", type=str, default="dataset/test.json")
+parser.add_argument("--eval_file", type=str, default="dataset/evaluate.json")
+parser.add_argument("--dict_path", type=str, default="models/wiki.vi.model.bin")
+parser.add_argument("--epochs", type=int, default=5)
+parser.add_argument("--lr", type=float, default=1e-3)
+parser.add_argument("--batch_size", type=int, default=16)
 
+args = parser.parse_args()
 
 if __name__ == "__main__":
     set_seed(42)
-    args = parse_args()
 
     pretrain_embed = KeyedVectors.load_word2vec_format(args.dict_path, binary=True)
 
     pretrained_dict = {'<pad>': 0, '<unk>': 1}
     for idx, word in enumerate(pretrain_embed.key_to_index, start=2):
         pretrained_dict[word] = idx
-        
-    train_data = Dataset(args.train_file, pretrained_dict)._return_tensor()
-    valid_data = Dataset(args.test_file, pretrained_dict)._return_tensor()
-    test_data = Dataset(args.eval_file, pretrained_dict)._return_tensor()
 
-    train_loader = DataLoader(train_data, shuffle=True, batch_size=args.batch_size)
-    valid_loader = DataLoader(valid_data, shuffle=False, batch_size=args.batch_size)
-    test_loader = DataLoader(test_data, shuffle=False, batch_size=args.batch_size)
+    train = Dataset(args.train_file)
+    valid = Dataset(args.test_file)
+    test = Dataset(args.test_file)
+
+    collator = DatasetCollator(pretrained_dict)
+
+    train_loader = DataLoader(train, shuffle=True, batch_size=args.batch_size, collate_fn=collator)
+    valid_loader = DataLoader(valid, shuffle=False, batch_size=args.batch_size, collate_fn=collator)
+    test_loader = DataLoader(test, shuffle=False, batch_size=args.batch_size, collate_fn=collator)
 
     if(torch.cuda.is_available()):
         print('Training on GPU.')
@@ -61,6 +61,8 @@ if __name__ == "__main__":
     cnn_model = CNN(pretrain_embed, vocab_size, output_size, embedding_dim, num_filters, kernel_sizes)
 
     print(cnn_model)
+    dtype = next(cnn_model.parameters()).dtype
+    print(f"dtype {dtype}")
 
     trainer = Trainer(
         model=cnn_model,
