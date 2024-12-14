ffd99a4e4aedbfca98eada09506170f4adb116b4
TrongNV2003 <trongnguyen220603@gmail.com>
2024-12-15 03:07:24 +0700

    fix: fix duplicate variables

--- training/evaluate.py
+++ training/evaluate.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import numpy as np
 import torch.nn as nn
@@ -17,23 +18,29 @@ class Tester:
 
     def test(self) -> None:
         test_losses = []
-        labels = []
-        preds = []
+        all_labels = []
+        all_preds = []
+        latencies = []
         num_correct = 0
 
         self.model.eval()
         for inputs, labels in self.test_loader:
             inputs, labels = inputs.to(self.device), labels.to(self.device)
 
+            start_time = time.time()
+
             output = self.model(inputs)
+
+            end_time = time.time()
+            latencies.append(end_time - start_time)
 
             test_loss = self.loss_fn(output.squeeze(), labels.float())
             test_losses.append(test_loss.item())
 
             pred = torch.round(output.squeeze())
 
-            labels.extend(labels.cpu().detach().numpy())
-            preds.extend(pred.cpu().detach().numpy())
+            all_labels.extend(labels.cpu().detach().numpy())
+            all_preds.extend(pred.cpu().detach().numpy())
 
             correct_tensor = pred.eq(labels.float().view_as(pred))
             correct = np.squeeze(correct_tensor.cpu().detach().numpy())
@@ -43,7 +50,8 @@ class Tester:
         test_acc = num_correct / len(self.test_loader.dataset)
         print("Accuracy: {:.3f}".format(test_acc))
 
-        self.f1(labels, preds, average="binary")
+        self.f1(all_labels, all_preds)
+        self.calculate_latency(latencies)
 
     @staticmethod
     def f1(label, predict):
@@ -51,9 +59,8 @@ class Tester:
         print(f"F1-score: {score*100:.3f}")
     
     @staticmethod
-    def calculate_latency(data):
-        process_times = [item["process_time"] for item in data]
-        p95_latency = np.percentile(process_times, 95)
-        average_latency = np.mean(process_times)
+    def calculate_latency(latencies):
+        p95_latency = np.percentile(latencies, 95)
+        average_latency = np.mean(latencies)
         print(f"P95 Latency: {p95_latency*1000:.3f} ms")
         print(f"Average: {average_latency*1000:.3f} ms")
