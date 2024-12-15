d6666c2af41e9ef49d70ef8e3f1745eb42e4b41a
TrongNV2003 <trongnguyen220603@gmail.com>
2024-12-15 13:09:59 +0700

    refactor: re-construct logically

--- training/preprocessing.py
+++ training/preprocessing.py
@@ -1,23 +1,27 @@
 import string
 
 class TextPreprocess:
-    def __init__(self):
-        dict = "stopwords/vietnamese-stopwords.txt"
-        self.stopwords_dict = dict
-    
-    def load_stopwords(self, file_path):
+    def __init__(self, text: str):
+        self.stopwords_file = "stopwords/vietnamese-stopwords.txt"
+        self.text = text
+
+    def process_text(self):
+        """Hàm chính để xử lý toàn bộ văn bản."""
+        cleaned_text = self._remove_punctuation(self.text)
+        cleaned_text = self._remove_stopwords(cleaned_text)
+        return cleaned_text
+
+    def _remove_stopwords(self, text):
+        stopwords = self._load_stopwords(self.stopwords_file)
+        words = text.split()
+        filtered_words = [word for word in words if word not in stopwords]
+        return " ".join(filtered_words)
+
+    def _load_stopwords(self, file_path):
         with open(file_path, 'r', encoding="utf-8") as f:
             stopwords = f.read().splitlines()
         return stopwords
 
-    def remove_stopwords(self, text):
-        stopwords = self.load_stopwords(self.stopwords_dict)
-        words = text.split()
-        words = [word for word in words if word not in stopwords]
-        return " ".join(words)
-
-    def process_text(self, text: str):
-        cleaned_text = text.translate(str.maketrans("", "", string.punctuation))
-        cleaned_text = cleaned_text.lower()
-        return cleaned_text
-
+    def _remove_punctuation(self, text):
+        cleaned_text = text.translate(str.maketrans(string.punctuation, " " * len(string.punctuation)))
+        return cleaned_text.lower()
